9a909ac099d77dcfc518a5133930dd35515d71a4
Victor Mendes <victor.sousa@grupobjl.com.br>
2024-03-21 10:49:11 -0300

    primeiro commit

---
+++ Atividade/app.py
@@ -0,0 +1,12 @@
+
+tempo = input("Informe quantos minutos de caminhada você faz por dia")
+tempo = tempo
+tempo = int(tempo)
+
+dias = input("Quantos dias da semana você caminha:")
+dias = dias
+dias = int(dias)
+
+resultado = dias * tempo
+
+print(f"O tempo que você caminha durante a semana e {resultado}")
